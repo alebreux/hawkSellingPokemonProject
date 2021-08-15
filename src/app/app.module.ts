e78cf48f7620c212e0437da06029111f7bdbb3ef
alebreux <alexandre.lebreux@gmail.com>
2021-08-15 10:41:04 +0200

    put tslint; add services; fic issues packages and launch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule, Routes} from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { SearchPageComponent } from './search-page/search-page.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {SearchPageComponent} from './search-page/search-page.component';
+
+const routes: Routes = [
+  {path: 'search', component: SearchPageComponent},
+  {path: '', redirectTo: '/search', pathMatch: 'full'},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SearchPageComponent
+    SearchPageComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/search-page/search-page.component.ts
+++ src/app/search-page/search-page.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
-  styleUrls: ['./search-page.component.css']
+  styleUrls: ['./search-page.component.css'],
 })
 export class SearchPageComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
